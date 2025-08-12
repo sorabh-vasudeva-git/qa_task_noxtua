cea9c87fc59da93592cc95e466f4e69fcc559fb7
sorabh-vasudeva-git <sorabh.vasudeva@gmail.com>
2025-08-13 00:31:37 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    setupNodeEvents(on, config) {
+      // If you don't have any event handlers, just return config
+      return config;
+    },
     baseUrl: "http://localhost:3000",
     specPattern: "cypress/e2e/**/*.cy.js",
     supportFile: "cypress/support/e2e.js",
